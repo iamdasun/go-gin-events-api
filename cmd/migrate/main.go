f988aba39c86d42fd35cf74158a5d7e59dc4c3bb
Dasun Wickramasooriya <32405325+dasunwickr@users.noreply.github.com>
2026-01-21 07:50:45 +0530

    feat: add migrations logic for sqlite3

---
+++ cmd/migrate/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+	"os"
+
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/sqlite3"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		log.Fatal("Please provide a migration direction: up | down")
+	}
+
+	direction := os.Args[1]
+
+	db, err := sql.Open("sqlite3", "./data.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	driver, err := sqlite3.WithInstance(db, &sqlite3.Config{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	m, err := migrate.NewWithDatabaseInstance(
+		"file://./migrations",
+		"sqlite3",
+		driver,
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer m.Close()
+
+	switch direction {
+	case "up":
+		err = m.Up()
+	case "down":
+		err = m.Down()
+	default:
+		log.Fatal("Invalid direction: use up or down")
+	}
+
+	if err != nil && err != migrate.ErrNoChange {
+		log.Fatal(err)
+	}
+
+	log.Println("Migration completed successfully")
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/iamdasun/go-gin-events-api
 
 go 1.25.5
+
+require github.com/golang-migrate/migrate/v4 v4.19.1
+
+require github.com/mattn/go-sqlite3 v1.14.22
