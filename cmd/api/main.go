2ea6081a960bb7309b63314753779bfcb1eea27d
Dasun Wickramasooriya <32405325+dasunwickr@users.noreply.github.com>
2026-01-21 16:48:05 +0530

    feat: add .gitignore for the project

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -6,16 +6,19 @@ import (
 
 	"github.com/iamdasun/go-gin-events-api/internal/database"
 	"github.com/iamdasun/go-gin-events-api/internal/env"
+	_ "github.com/joho/godotenv/autoload"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 type application struct {
-	port int
+	port      int
 	jwtSecret string
-	models database.Models
+	models    database.Models
 }
+
 func main() {
 	db, err := sql.Open("sqlite3", "./data.db")
-	
+
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -24,9 +27,9 @@ func main() {
 
 	models := database.NewModels(db)
 	app := &application{
-		port: env.GetEnvInt("PORT", 8080),
+		port:      env.GetEnvInt("PORT", 8080),
 		jwtSecret: env.GetEnvString("JWT_SECRET", "some-secret-1234567"),
-		models: models,
+		models:    models,
 	}
 
 	if err := app.serve(); err != nil {
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require github.com/golang-migrate/migrate/v4 v4.19.1
 
 require (
 	github.com/gin-gonic/gin v1.11.0
+	github.com/joho/godotenv v1.5.1
 	github.com/mattn/go-sqlite3 v1.14.22
 )
 
